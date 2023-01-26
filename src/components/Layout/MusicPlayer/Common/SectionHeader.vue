f50caf0f855de5961f99953d15856a83b695382a
sakusaku000 <harry.spncr@gmail.com>
2023-01-26 16:23:28 +0000

    fixed size of section header icon

--- src/components/Layout/MusicPlayer/Common/SectionHeader.vue
+++ src/components/Layout/MusicPlayer/Common/SectionHeader.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="select-none mb-3">
         <div class="flex items-center gap-4 mb-3" :class="`${(!icon) ? 'my-0' : ''}`">
-            <img src="/assets/image/cd.png" alt="CD Section Header Icon" draggable="false" v-if="icon"> 
+            <img class="w-[48px] h-[48px]" src="/assets/image/cd.png" alt="CD Section Header Icon" draggable="false" v-if="icon"> 
             <h2 class="text-[16px] mt-1" :class="`${(!icon) ? 'mb-2' : ''}`"><slot/></h2>
         </div>
         <hr class="border-t-windows-gray-main">
