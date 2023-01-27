b3a63732c4b826bf10f7c509729774eec48ebcc9
sakusaku000 <harry.spncr@gmail.com>
2023-01-27 10:15:14 +0000

    added album total

--- src/views/Albums.vue
+++ src/views/Albums.vue
@@ -1,5 +1,5 @@
 <template>
-    <SectionHeader :icon="true">Albums</SectionHeader>
+    <SectionHeader :icon="true" :total="$store.state.library.albums.list.length">Albums</SectionHeader>
     <AlbumList>
         <AlbumEntry
             v-for="album in $store.state.library.albums.list" 
@@ -10,19 +10,22 @@
             :tracks="album.album.tracks.items"
         />
     </AlbumList>
+    <LoadMore v-if="$store.state.library.albums.list.length > 50" type="albums"/>
 </template>
 
 <script>
 import SectionHeader from '../components/MusicPlayer/Common/SectionHeader.vue';
 import AlbumList from '../components/MusicPlayer/Library/AlbumList/AlbumList.vue';
 import AlbumEntry from '../components/MusicPlayer/Library/AlbumList/AlbumEntry.vue';
+import LoadMore from '../components/MusicPlayer/Library/LoadMore.vue';
 
 export default {
     name:"AlbumsView",
     components:{
         SectionHeader,
         AlbumList,
-        AlbumEntry
+        AlbumEntry,
+        LoadMore
     }
 }
 </script>
